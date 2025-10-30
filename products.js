7d131ab243ac750902d141f81019a19a6b96c718
TheQuietOne409 <922346409@sfsu.edu>
2025-10-29 21:06:44 -0700

    Update product details and images in products.js

---
+++ products.js
@@ -0,0 +1,22 @@
+const products = {
+  Coffee: [
+    { name: "Espresso Blend", price: "$12.99", image: "Coffee.png" },
+    { name: "Dark Roast", price: "$11.99", image: "Coffee2.png" },
+    { name: "Decaf", price: "$10.99", image: "Coffee.png" }
+  ],
+  Tea: [
+    { name: "Green Tea", price: "$7.99", image: "Tea.png" },
+    { name: "Pepperming Tea", price: "$5.99", image: "Tea.png" },
+    { name: "Chai", price: "$8.99", image: "Tea.png" }
+  ],
+  Mugs: [
+    { name: "Classic Mug", price: "$9.99", image: "Mug.png" },
+    { name: "Travel Mug", price: "$12.99", image: "BrewUp.jpeg" },
+    { name: "Camping Mug", price: "$10.99", image: "CampingMug.png" }
+  ], 
+   Apparel: [
+    { name: "BrewUp T-Shirt", price: "$19.99", image: "tshirt.jpg" },
+    { name: "BrewUp Hoodie", price: "$29.99", image: "Hoodie.png" },
+    { name: "BrewUp Beanie", price: "$9.99", image: "BrewUp.jpeg" }
+  ]
+};
