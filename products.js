19ad132bb0faba1998fd58282becfd390509a484
TheQuietOne409 <922346409@sfsu.edu>
2025-11-03 11:34:43 -0800

    Update products.js

--- products.js
+++ products.js
@@ -1,22 +1,22 @@
 const products = {
   Coffee: [
-    { name: "Espresso Blend", price: "$12.99", image: "Images/Coffee.png" },
-    { name: "Dark Roast", price: "$11.99", image: "Images/Coffee2.png" },
-    { name: "Decaf", price: "$10.99", image: "Images/Coffee.png" }
+    { name: "Espresso Blend", price: "$12.99", img.src = "Images/Coffee.png" },
+    { name: "Dark Roast", price: "$11.99", img.src = "Images/Coffee2.png" },
+    { name: "Decaf", price: "$10.99", img.src = "Images/Coffee.png" }
   ],
   Tea: [
-    { name: "Green Tea", price: "$7.99", image: "Images/Tea.png" },
-    { name: "Pepperming Tea", price: "$5.99", image: "Images/Tea.png" },
-    { name: "Chai", price: "$8.99", image: "Images/Tea.png" }
+    { name: "Green Tea", price: "$7.99", img.src = "Images/Tea.png" },
+    { name: "Pepperming Tea", price: "$5.99", img.src = "Images/Tea.png" },
+    { name: "Chai", price: "$8.99", img.src = "Images/Tea.png" }
   ],
   Mugs: [
-    { name: "Classic Mug", price: "$9.99", image: "Images/Mug.png" },
-    { name: "Travel Mug", price: "$12.99", image: "Images/BrewUp.jpeg" },
-    { name: "Camping Mug", price: "$10.99", image: "Images/CampingMug.png" }
+    { name: "Classic Mug", price: "$9.99", img.src = "Images/Mug.png" },
+    { name: "Travel Mug", price: "$12.99", img.src = "Images/BrewUp.jpeg" },
+    { name: "Camping Mug", price: "$10.99", img.src = "Images/CampingMug.png" }
   ], 
    Apparel: [
-    { name: "BrewUp T-Shirt", price: "$19.99", image: "Images/tshirt.jpg" },
-    { name: "BrewUp Hoodie", price: "$29.99", image: "Images/Hoodie.png" },
-    { name: "BrewUp Beanie", price: "$9.99", image: "Images/BrewUp.jpeg" }
+    { name: "BrewUp T-Shirt", price: "$19.99", img.src = "Images/tshirt.jpg" },
+    { name: "BrewUp Hoodie", price: "$29.99", img.src = "Images/Hoodie.png" },
+    { name: "BrewUp Beanie", price: "$9.99", img.src = "Images/BrewUp.jpeg" }
   ]
 };
